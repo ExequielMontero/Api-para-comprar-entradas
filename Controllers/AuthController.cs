1838cddc15cf1c9838e199027c253dcf722b1031
ExequielMontero <exeee2015@gmail.com>
2025-05-17 16:05:42 -0300

    creacion de controlador de Auth y Roles

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Api_entradas.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_entradas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController
+    {
+
+        [HttpPost("register")] public async Task<IActionResult> Register(RegisterDto dto) { /* hashear, guardar user con rol Client */ }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            // Validar credenciales, generar JWT, retornar { token }
+        }
+    }
